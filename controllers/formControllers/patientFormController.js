e2c7b4dab675d3dcc7a6052f50fbf058de58e11c
silaserter <silasserter@gmail.com>
2023-02-15 05:43:25 +0300

    patient forms insert fixed

--- controllers/formControllers/patientFormController.js
+++ controllers/formControllers/patientFormController.js
@@ -7,7 +7,6 @@ exports.insertPatientForm = (req, res, next) => {
         return next(new AppError("No form data found", 404));
 
     const values = [
-        req.body.ID,
         req.body.studentID,
         req.body.studentName,
         req.body.rotationID,
@@ -47,7 +46,41 @@ exports.insertPatientForm = (req, res, next) => {
     console.log(req.body);
 
     conn.query(
-        "INSERT INTO patientreports (ID,name, surname, courses, rotationNo, previousRotationNo) VALUES(?)",
+        "INSERT INTO patientreports (studentID," +
+        "studentName," +
+        "rotationID," +
+        "specialtyID," +
+        "attendingPhysicianID," +
+        "diagnosisID," +
+        "diagnosis," +
+        "patientHospitalID," +
+        "patientName," +
+        "patientGender," +
+        "patientAge," +
+        "relevants," +
+        "keySymptoms," +
+        "signs," +
+        "data," +
+        "traineesRole," +
+        "levelOfCare," +
+        "setting," +
+        "tier1ID," +
+        "tier1," +
+        "tier2ID," +
+        "tier2," +
+        "tier3ID," +
+        "tier3," +
+        "tier4ID," +
+        "tier4," +
+        "lastSaveDate," +
+        "lastSaveTime," +
+        "approveDate," +
+        "approveTime," +
+        "isSent," +
+        "isApproved," +
+        "comment," +
+        "localStorageID) " +
+        "VALUES(?)",
         [values],
         function (err, data, fields) {
             if (err)
@@ -55,25 +88,6 @@ exports.insertPatientForm = (req, res, next) => {
             res.status(201).json({
                 status: "success",
                 message: "New student added!",
-            });
-        }
-    );
-};
-
-exports.getPatientFormWithID = (req, res, next) => {
-    //check if the id is specified in the request parameter,
-    if (!req.params.ID) {
-        return next(new AppError("No student with this ID found", 404));
-    }
-    conn.query(
-        "SELECT * FROM patientreports WHERE ID = ?",
-        [req.params.ID],
-        function (err, data, fields) {
-            if (err) return next(new AppError(err, 500));
-            res.status(200).json({
-                status: "success",
-                length: data?.length,
-                data: data,
             });
         }
     );
@@ -93,13 +107,312 @@ exports.getAllPatientForms = (req, res, next) => {
     );
 };
 
-exports.updateSPatientFormWithID = (req, res, next) => {
+exports.getPatientFormWithID = (req, res, next) => {
+    if (!req.params.ID) {
+        return next(new AppError("No patient with this ID found", 404));
+    }
+    conn.query(
+        "SELECT * FROM patientreports WHERE ID = ?",
+        [req.params.ID],
+        function (err, data, fields) {
+            if (err) return next(new AppError(err, 500));
+            res.status(200).json({
+                status: "success",
+                length: data?.length,
+                data: data,
+            });
+        }
+    );
+};
+
+exports.updatePatientFormWithID = (req, res, next) => {
     if (!req.params.ID) {
         return next(new AppError("No todo id found", 404));
     }
+
+    console.log("ID: " + req.params.ID);
+    console.log("Student ID: " + req.params.studentID);
+    console.log("Student Name: " + req.params.studentName);
+    console.log("Rotation ID: " + req.params.rotationID);
+    console.log("Specialty ID: " + req.params.specialtyID);
+
+    var firstArgumentEntered = false;
+    var queryString = "UPDATE patientreports SET ";
+
+    if (req.params.studentID !== undefined) {
+        queryString += "studentID=" + req.params.studentID;
+        firstArgumentEntered = true;
+    }
+
+    if (req.params.studentName !== undefined) {
+        if (firstArgumentEntered) {
+            queryString += ", ";
+        }
+        queryString += "studentName= '" + req.params.studentName + "'";
+        firstArgumentEntered = true;
+    }
+
+    if (req.params.rotationID !== undefined) {
+        if (firstArgumentEntered) {
+            queryString += ", ";
+        }
+        queryString += "rotationID= '" + req.params.rotationID + "'";
+        firstArgumentEntered = true;
+    }
+
+    if (req.params.specialtyID !== undefined) {
+        if (firstArgumentEntered) {
+            queryString += ", ";
+        }
+        queryString += "specialtyID= '" + req.params.specialtyID + "'";
+        firstArgumentEntered = true;
+    }
+
+    if (req.params.attendingPhysicianID !== undefined) {
+        if (firstArgumentEntered) {
+            queryString += ", ";
+        }
+        queryString += "attendingPhysicianID= '" + req.params.attendingPhysicianID + "'";
+        firstArgumentEntered = true;
+    }
+
+    if (req.params.diagnosisID !== undefined) {
+        if (firstArgumentEntered) {
+            queryString += ", ";
+        }
+        queryString += "diagnosisID= '" + req.params.diagnosisID + "'";
+        firstArgumentEntered = true;
+    }
+
+    if (req.params.diagnosis !== undefined) {
+        if (firstArgumentEntered) {
+            queryString += ", ";
+        }
+        queryString += "diagnosis= '" + req.params.diagnosis + "'";
+        firstArgumentEntered = true;
+    }
+
+    if (req.params.patientHospitalID !== undefined) {
+        if (firstArgumentEntered) {
+            queryString += ", ";
+        }
+        queryString += "patientHospitalID= '" + req.params.patientHospitalID + "'";
+        firstArgumentEntered = true;
+    }
+
+    if (req.params.patientName !== undefined) {
+        if (firstArgumentEntered) {
+            queryString += ", ";
+        }
+        queryString += "patientName= '" + req.params.patientName + "'";
+        firstArgumentEntered = true;
+    }
+
+    if (req.params.patientGender !== undefined) {
+        if (firstArgumentEntered) {
+            queryString += ", ";
+        }
+        queryString += "patientGender= '" + req.params.patientGender + "'";
+        firstArgumentEntered = true;
+    }
+
+    if (req.params.patientAge !== undefined) {
+        if (firstArgumentEntered) {
+            queryString += ", ";
+        }
+        queryString += "patientAge= '" + req.params.patientAge + "'";
+        firstArgumentEntered = true;
+    }
+
+    if (req.params.relevants !== undefined) {
+        if (firstArgumentEntered) {
+            queryString += ", ";
+        }
+        queryString += "relevants= '" + req.params.relevants + "'";
+        firstArgumentEntered = true;
+    }
+
+    if (req.params.keySymptoms !== undefined) {
+        if (firstArgumentEntered) {
+            queryString += ", ";
+        }
+        queryString += "keySymptoms= '" + req.params.keySymptoms + "'";
+        firstArgumentEntered = true;
+    }
+
+    if (req.params.signs !== undefined) {
+        if (firstArgumentEntered) {
+            queryString += ", ";
+        }
+        queryString += "signs= '" + req.params.signs + "'";
+        firstArgumentEntered = true;
+    }
+
+    if (req.params.data !== undefined) {
+        if (firstArgumentEntered) {
+            queryString += ", ";
+        }
+        queryString += "data= '" + req.params.data + "'";
+        firstArgumentEntered = true;
+    }
+
+    if (req.params.traineesRole !== undefined) {
+        if (firstArgumentEntered) {
+            queryString += ", ";
+        }
+        queryString += "traineesRole= '" + req.params.traineesRole + "'";
+        firstArgumentEntered = true;
+    }
+
+    if (req.params.levelOfCare !== undefined) {
+        if (firstArgumentEntered) {
+            queryString += ", ";
+        }
+        queryString += "levelOfCare= '" + req.params.levelOfCare + "'";
+        firstArgumentEntered = true;
+    }
+
+    if (req.params.setting !== undefined) {
+        if (firstArgumentEntered) {
+            queryString += ", ";
+        }
+        queryString += "setting= '" + req.params.setting + "'";
+        firstArgumentEntered = true;
+    }
+
+    if (req.params.tier1ID !== undefined) {
+        if (firstArgumentEntered) {
+            queryString += ", ";
+        }
+        queryString += "tier1ID= '" + req.params.tier1ID + "'";
+        firstArgumentEntered = true;
+    }
+
+    if (req.params.tier1 !== undefined) {
+        if (firstArgumentEntered) {
+            queryString += ", ";
+        }
+        queryString += "tier1= '" + req.params.tier1 + "'";
+        firstArgumentEntered = true;
+    }
+
+    if (req.params.tier2 !== undefined) {
+        if (firstArgumentEntered) {
+            queryString += ", ";
+        }
+        queryString += "tier2= '" + req.params.tier2 + "'";
+        firstArgumentEntered = true;
+    }
+
+    if (req.params.tier2 !== undefined) {
+        if (firstArgumentEntered) {
+            queryString += ", ";
+        }
+        queryString += "tier3ID= '" + req.params.tier3ID + "'";
+        firstArgumentEntered = true;
+    }
+
+    if (req.params.tier3ID !== undefined) {
+        if (firstArgumentEntered) {
+            queryString += ", ";
+        }
+        queryString += "tier3ID= '" + req.params.tier3ID + "'";
+        firstArgumentEntered = true;
+    }
+
+    if (req.params.tier3 !== undefined) {
+        if (firstArgumentEntered) {
+            queryString += ", ";
+        }
+        queryString += "tier3= '" + req.params.tier3 + "'";
+        firstArgumentEntered = true;
+    }
+
+    if (req.params.tier4ID !== undefined) {
+        if (firstArgumentEntered) {
+            queryString += ", ";
+        }
+        queryString += "tier4ID= '" + req.params.tier4ID + "'";
+        firstArgumentEntered = true;
+    }
+
+    if (req.params.tier4 !== undefined) {
+        if (firstArgumentEntered) {
+            queryString += ", ";
+        }
+        queryString += "tier4= '" + req.params.tier4 + "'";
+        firstArgumentEntered = true;
+    }
+
+    if (req.params.lastSaveDate !== undefined) {
+        if (firstArgumentEntered) {
+            queryString += ", ";
+        }
+        queryString += "lastSaveDate= '" + req.params.lastSaveDate + "'";
+        firstArgumentEntered = true;
+    }
+
+    if (req.params.lastSaveTime !== undefined) {
+        if (firstArgumentEntered) {
+            queryString += ", ";
+        }
+        queryString += "lastSaveTime= '" + req.params.lastSaveTime + "'";
+        firstArgumentEntered = true;
+    }
+
+    if (req.params.approveDate !== undefined) {
+        if (firstArgumentEntered) {
+            queryString += ", ";
+        }
+        queryString += "approveDate= '" + req.params.approveDate + "'";
+        firstArgumentEntered = true;
+    }
+
+    if (req.params.approveTime !== undefined) {
+        if (firstArgumentEntered) {
+            queryString += ", ";
+        }
+        queryString += "approveTime= '" + req.params.approveTime + "'";
+        firstArgumentEntered = true;
+    }
+
+    if (req.params.isSent !== undefined) {
+        if (firstArgumentEntered) {
+            queryString += ", ";
+        }
+        queryString += "isSent= '" + req.params.isSent + "'";
+        firstArgumentEntered = true;
+    }
+
+    if (req.params.isApproved !== undefined) {
+        if (firstArgumentEntered) {
+            queryString += ", ";
+        }
+        queryString += "isApproved= '" + req.params.isApproved + "'";
+        firstArgumentEntered = true;
+    }
+
+    if (req.params.comment !== undefined) {
+        if (firstArgumentEntered) {
+            queryString += ", ";
+        }
+        queryString += "comment= '" + req.params.comment + "'";
+        firstArgumentEntered = true;
+    }
+
+    if (req.params.localStorageID !== undefined) {
+        if (firstArgumentEntered) {
+            queryString += ", ";
+        }
+        queryString += "localStorageID= '" + req.params.localStorageID + "'";
+        firstArgumentEntered = true;
+    }
+
+    queryString += " WHERE ID=" + req.params.ID;
+    console.log("THIS IS THE QUERY: " + queryString);
+
     conn.query(
-        "UPDATE patientreports SET rotationNo=? WHERE ID=?",
-        [req.params.rotationNo, req.params.ID],
+        queryString,
         function (err, data, fields) {
             if (err) return next(new AppError(err, 500));
             res.status(201).json({
@@ -108,6 +421,8 @@ exports.updateSPatientFormWithID = (req, res, next) => {
             });
         }
     );
+
+
 };
 
 exports.deletePatientFormWithID = (req, res, next) => {
--- routes/patientForm.js
+++ routes/patientForm.js
@@ -3,20 +3,55 @@ const patientFormController = require("../controllers/formControllers/patientFor
 const router = express.Router();
 
 //router.route('/all').get(stdController.getAllStudents).post(stdController.insertStd);
+router.route("/insert").put(patientFormController.insertPatientForm);
+
 router
-    .route("/all")
+    .route("/get/all")
     .get(patientFormController.getAllPatientForms);
 
-router.route("/insert").put(patientFormController.insertPatientForm)
-
+router
+    .route("/get/:ID")
+    .get(patientFormController.getPatientFormWithID);
 
 router
-    .route("/:ID")
-    .get(patientFormController.getPatientFormWithID)
-    .put(patientFormController.updateSPatientFormWithID)
+    .route("/update/:ID" +
+        "/sID/:studentID?" +
+        "/sName/:studentName?" +
+        "/rID/:rotationID?" +
+        "/sID/:specialtyID?" +
+        "/aID/:attendingPhysicianID?" +
+        "/dID/:diagnosisID?" +
+        "/diagnosis/:diagnosis?" +
+        "/phID/:patientHospitalID?" +
+        "/pName/:patientName?" +
+        "/pAge/:patientAge?" +
+        "/r/:relevants?" +
+        "/ks/:keySymptoms?" +
+        "/s/:signs?" +
+        "/d/:data?" +
+        "/tr/:traineesRole?" +
+        "/loc/:levelOfCare?" +
+        "/setting/:setting?" +
+        "/t1ID/:tier1ID?" +
+        "/t1/:tier1?" +
+        "/t2ID/:tier2ID?" +
+        "/t2/:tier2?" +
+        "/t3ID/:tier3ID?" +
+        "/t3/:tier3?" +
+        "/t4D/:tier4D?" +
+        "/t4/:tier4?" +
+        "/lsd/:lastSaveDate?" +
+        "/lst/:lastSaveTime?" +
+        "/ad/:approveDate?" +
+        "/at/:approveTime?" +
+        "/issent/:isSent?" +
+        "/isapproved/:isApproved?" +
+        "/comment/:comment?" +
+        "/lsid/:localStorageID?")
+    .put(patientFormController.updatePatientFormWithID)
+
+router
+    .route("/delete/:ID")
     .delete(patientFormController.deletePatientFormWithID);
-
-
-
 
 module.exports = router;
