ee1b68b3b1d63747649d51965d868ec36381b750
ecehavanci <ecehavanci@gmail.com>
2023-02-21 22:31:10 +0300

    diagnosis API added

--- controllers/rootController.js
+++ controllers/rootController.js
@@ -1,6 +1,21 @@
 const AppError = require("../utils/appError");
 const conn = require("../services/db");
 
+//attending physc
 exports.login = (req, res, next) => {
-    
+    if (!req.params.phoneOrID) {
+        return next(new AppError("No attending physician with this ID found", 404));
+    }
+    conn.query(
+        "SELECT * FROM attendingphysicians WHERE ID = ?",
+        [req.params.ID],
+        function (err, data, fields) {
+            if (err) return next(new AppError(err, 500));
+            res.status(200).json({
+                status: "success",
+                length: data?.length,
+                data: data,
+            });
+        }
+    );
 }
