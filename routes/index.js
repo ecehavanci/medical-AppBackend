e657e5d2bba4c16e1f52b93351ecb8126392b3b9
Ece Havanci <ecehavanci@gmail.com>
2023-09-26 15:31:14 +0300

    sdsdsd

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ const specialties = require("./specialties");
 const course = require("./course");
 const rotations = require("./rotations");
 const web = require("./web");
-const procedure = require("./procedure");
+const procedureRoutes = require("./routes/procedureRoutes");
 const router = express();
 
 
@@ -23,7 +23,7 @@ router.use("/specialties", specialties);
 router.use("/courses", course);
 router.use("/rotations", rotations);
 router.use("/web", web);
-router.use("/procedure", procedure);
+router.use("/procedure", procedureRoutes);
 // router.route("student").get(controllers.getAllStudents)
 // // .post(controllers.insertStd);
 
--- routes/procedure.js
+++ routes/procedure.js
@@ -1,21 +1,22 @@
 const express = require("express");
-const procedureController= require("../controllers/procedureController");
+const procedureController = require("../controllers/procedureController");
 const router = express.Router();
 
 router
     .route("/approved")
     .get(procedureController.getApprovedProcedures)
-    
+
 router
     .route("/:procedureID")
     .get(procedureController.getProceduresByID) //NOT USING CURRENTLY
-    
+
 router
     .route("/:relatedReport")
     .get(procedureController.getProcedureByRelatedReportID)
     .put(procedureController.updateProcedure);
 
+router.route("/insert")
+    .post(procedureController.insertProcedure);
 
-router.route("/insert").post(procedureController.insertProcedure);
 
 module.exports = router;
