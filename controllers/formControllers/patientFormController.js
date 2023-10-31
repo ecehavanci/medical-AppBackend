64f6f75e630f375a4a659e802c51dc557339d644
Ece Havanci <ecehavanci@gmail.com>
2023-10-31 16:49:29 +0300

    a

--- controllers/formControllers/patientFormController.js
+++ controllers/formControllers/patientFormController.js
@@ -1,8 +1,7 @@
-import { config } from './config.js';
+import * as config from './config.js';
 const AppError = require("../../utils/appError");
 const conn = require("../../services/db");
 const { query } = require("express");
-const config = require("../../config");
 const currentYear = config.year;
 const currentSeason = config.season;
 
