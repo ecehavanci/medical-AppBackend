0c4a23838f003228e9d0890dc1fc8a24cfa9cfbb
Ece Havanci <ecehavanci@gmail.com>
2023-11-01 15:04:40 +0300

    DFDGFD

--- config.js
+++ config.js
@@ -1,9 +1,18 @@
+const timestamp = Date.now(); // Get the current timestamp
+
+const date = new Date(timestamp); // Convert the timestamp to a Date object
+
+const year = date.getFullYear(); // Get the year (4 digits)
+const month = String(date.getMonth() + 1).padStart(2, '0'); // Get the month (0-11), add 1, and format as 2 digits
+const day = String(date.getDate()).padStart(2, '0'); // Get the day of the month and format as 2 digits
+
+const formattedDate = `${year}-${month}-${day}`; // Format the date as 'Y-M-D'
 
 module.exports = {
     app: {
         year: 2023,
         season: 1,
-        date: Date.now(),
+        date: formattedDate,
     },
     db: {
         host: 'localhost',
