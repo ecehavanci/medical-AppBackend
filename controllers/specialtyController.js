4ffcefa3f5c1ac67bc94cc484a359e0a879aad65
Ece Havanci <ecehavanci@gmail.com>
2023-11-01 11:30:33 +0300

    config path refactored

--- controllers/specialtyController.js
+++ controllers/specialtyController.js
@@ -1,6 +1,6 @@
 const AppError = require("../utils/appError");
 const conn = require("../services/db");
-const config = require("../../config");
+const config = require("../config.js");
 const currentYear = config.app.year;
 const currentSeason = config.app.season;
 const currentDate = config.app.date;
