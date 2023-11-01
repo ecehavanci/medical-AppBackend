bfb9eff19065a3c17b2c7afc5bd46317f332c04f
Ece Havanci <ecehavanci@gmail.com>
2023-11-01 09:53:24 +0300

    student isnertion fixed

--- controllers/stdController.js
+++ controllers/stdController.js
@@ -10,8 +10,7 @@ exports.insertStd = (req, res, next) => {
     if (!req.body)
         return next(new AppError("No form data found", 404));
 
-    const values = [req.body.ID, req.body.name, req.body.surname,
-    req.body.courses];
+    const values = [req.body.ID, req.body.name, req.body.surname];
 
     console.log(req.body);
 
