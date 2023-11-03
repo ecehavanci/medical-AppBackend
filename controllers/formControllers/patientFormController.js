e5783e500eef4639df5caa064daf1278c8dba2e0
Ece Havanci <ecehavanci@gmail.com>
2023-11-03 11:26:12 +0300

    searchPatientFormsByMultipleAcceptance updated

--- controllers/formControllers/patientFormController.js
+++ controllers/formControllers/patientFormController.js
@@ -578,7 +578,7 @@ exports.searchPatientFormsByMultipleAcceptance = (req, res, next) => {
         WHERE studentID = ?
         AND isSent = ?
         AND courseID = ?
-        AND (isApproved = ? OR isApproved = ?)
+        AND (pr.isApproved IN (?, ?))
         AND year = ?
         AND season = ?
         AND UPPER(illnessScript) LIKE ?
