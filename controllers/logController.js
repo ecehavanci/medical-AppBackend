32562d6984179e1704a63f5f3e63aaf9193ae3c8
Ece Havanci <ecehavanci@gmail.com>
2023-11-06 10:30:46 +0300

    sdfsd

--- controllers/logController.js
+++ controllers/logController.js
@@ -5,7 +5,7 @@ const currentYear = config.app.year;
 const currentSeason = config.app.season;
 const currentDate = config.app.date;
 
-exports.updatePatientFormLog = (updateFields, values) => {
+exports.updatePatientFormLog = (updateFields, values) => { //todo fix the query and handle the case when patient_logger increments the change_no
     return new Promise((resolve, reject) => {
         const valuesCopy = values.slice(); // Create a copy of the values array
         const columnNamesCopy = updateFields.slice(); // Create a copy of the column names array
@@ -27,44 +27,46 @@ exports.updatePatientFormLog = (updateFields, values) => {
             }
 
             // Create a query to select the old data for the given ID
-            const placeholders = columnNamesCopy.join(', '); 
+            const placeholders = columnNamesCopy.join(', ');
             const selectOldDataQuery = `SELECT ${placeholders} FROM patientreports WHERE ID = ? AND isApproved = 2`;
 
             console.log(selectOldDataQuery);
 
-            conn.query(selectOldDataQuery, [ID], (err, oldDataRows) => {
-                if (err) {
-                    console.error("Error reading old data:", err);
-                    reject(err);
-                }
-                else if (oldDataRows.length === 0) {
-                    // Handle the case where no data is found for the given ID
-                    const errorMessage = "No data found for the provided ID";
-                    console.error(errorMessage);
-                    resolve();
-                }
-                else {
-                    // Assuming oldDataRows is an array of rows with the old data
-                    // Create a JSON object to represent the old and new data
-                    const logData = {
-                        old_data: oldDataRows[0], // Assuming you're interested in the first row
-                        new_data: newJSON, // Remove the last ID value
-                    };
-
-                    // Now, you can insert the logData into your logger table
-                    const insertLogDataQuery = "INSERT INTO patient_logger (old_data, new_data) VALUES (?, ?)";
-
-                    conn.query(insertLogDataQuery, [JSON.stringify(logData.old_data), JSON.stringify(logData.new_data)], (err) => {
-                        if (err) {
-                            console.error("Error inserting log data:", err);
-                            reject(err);
-                        }
-
-                        console.log("Log data inserted:", logData);
+            conn.query(selectOldDataQuery,
+                [valuesCopy, ID],
+                (err, oldDataRows) => {
+                    if (err) {
+                        console.error("Error reading old data:", err);
+                        reject(err);
+                    }
+                    else if (oldDataRows.length === 0) {
+                        // Handle the case where no data is found for the given ID
+                        const errorMessage = "No data found for the provided ID";
+                        console.error(errorMessage);
                         resolve();
-                    });
-                }
-            });
+                    }
+                    else {
+                        // Assuming oldDataRows is an array of rows with the old data
+                        // Create a JSON object to represent the old and new data
+                        const logData = {
+                            old_data: oldDataRows[0], // Assuming you're interested in the first row
+                            new_data: newJSON, // Remove the last ID value
+                        };
+
+                        // Now, you can insert the logData into your logger table
+                        const insertLogDataQuery = "INSERT INTO patient_logger (old_data, new_data) VALUES (?, ?)";
+
+                        conn.query(insertLogDataQuery, [JSON.stringify(logData.old_data), JSON.stringify(logData.new_data)], (err) => {
+                            if (err) {
+                                console.error("Error inserting log data:", err);
+                                reject(err);
+                            }
+
+                            console.log("Log data inserted:", logData);
+                            resolve();
+                        });
+                    }
+                });
         }
     });
 }
