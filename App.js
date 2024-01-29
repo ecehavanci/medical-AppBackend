f0b0a636a01f88cdcad416f6f058f620c0fe4a5b
Ece Havanci <ecehavanci@gmail.com>
2024-01-29 15:18:36 +0300

    helpmet typo error

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ app.use(errorHandler);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(api, routes);
-app.use(api, helmet);
+app.use(api, helmet());
 
 app.get("/", (req, res) => {
     res.send("working");
