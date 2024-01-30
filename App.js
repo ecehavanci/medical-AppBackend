88f1eefde8209065fd04aa23780ca8a02251a091
Ece Havanci <ecehavanci@gmail.com>
2024-01-30 16:41:28 +0300

    app.js from prev commits

--- App.js
+++ App.js
@@ -1,37 +1,25 @@
 const express = require("express");
-const helmet = require("helmet");
 const cors = require("cors");
 const routes = require("./routes");
+const AppError = require("./utils/appError");
 const errorHandler = require("./utils/errorHandler");
+var connect = require('connect');
 const app = express();
 const api = "/";
 var bodyParser = require('body-parser');
-const http = require('http');
-const PORT = 443;
+const killPort = require('kill-port');
 
 app.use(errorHandler);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(api, routes);
-app.use(api, helmet());
-// app.use(cors({
-//   origin: 'https://medsis.izmirekonomi.edu.tr',
-//   methods: ['GET', 'POST', 'DELETE', 'PUT'],
-//   allowedHeaders: ['Content-Type', 'Authorization']
-// }));
-app.disable('x-powered-by');
-
 app.get("/", (req, res) => {
-  res.send("working");
+    res.send("working");
 });
 
-const server = http.createServer(app);
-
-server.keepAliveTimeout = 60000; // 60 saniye
-
-server.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
-
+const PORT = 443;
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+  });
 
 module.exports = app;
