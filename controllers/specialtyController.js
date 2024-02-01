a78f94d2b6e63dd2bbf9e3c09824556f63d069a2
Ece Havanci <ecehavanci@gmail.com>
2024-02-01 13:51:12 +0300

    specialty end points bareer token added

--- controllers/specialtyController.js
+++ controllers/specialtyController.js
@@ -6,85 +6,108 @@ const currentSeason = config.app.season;
 const currentDate = config.app.date;
 
 exports.getAllSpecialties = (req, res, next) => { //all specialties in DB
-
-    conn.query(
-        "select * from specialties",
-        [req.params.studentID],
-        function (err, data, fields) {
-            if (err) return next(new AppError(err, 500));
-            res.status(200).json({
-                status: "success",
-                length: data?.length,
-                data: data,
-            });
-        }
-    );
+    try {
+        verifyToken(req, res, () => {
+            conn.query(
+                "select * from specialties",
+                [req.params.studentID],
+                function (err, data, fields) {
+                    if (err) return next(new AppError(err, 500));
+                    res.status(200).json({
+                        status: "success",
+                        length: data?.length,
+                        data: data,
+                    });
+                }
+            );
+        });
+    } catch (error) {
+        return next(new AppError(error.message, 500));
+    }
 }
 
 exports.getCourseSpecialties = (req, res, next) => { //current course specialty
-    const queryString = `
-    select sp.ID, sp.description
-    from student s
-             left join enrollment e on e.std_id = s.ID
-             left join rotation_courses rc on rc.rotation_id = e.rotation_id
-             left join intervals i on i.ID = rc.interval_id
-             left join specialties sp on sp.course_ID = rc.course_id or sp.ID = -1
-    where i.year = ?
-      and i.season = ?
-      and s.ID = ?
-      and current_date between i.start and i.end;
-    `;
+    try {
+        verifyToken(req, res, () => {
+            const queryString = `
+            select sp.ID, sp.description
+            from student s
+                    left join enrollment e on e.std_id = s.ID
+                    left join rotation_courses rc on rc.rotation_id = e.rotation_id
+                    left join intervals i on i.ID = rc.interval_id
+                    left join specialties sp on sp.course_ID = rc.course_id or sp.ID = -1
+            where i.year = ?
+            and i.season = ?
+            and s.ID = ?
+            and current_date between i.start and i.end;
+            `;
 
-    conn.query(
-        queryString,
-        [currentYear, currentSeason, req.params.studentID, currentDate],
-        function (err, data, fields) {
-            if (err) return next(new AppError(err, 500));
-            res.status(200).json({
-                status: "success",
-                length: data?.length,
-                data: data,
-            });
-        }
-    );
+            conn.query(
+                queryString,
+                [currentYear, currentSeason, req.params.studentID, currentDate],
+                function (err, data, fields) {
+                    if (err) return next(new AppError(err, 500));
+                    res.status(200).json({
+                        status: "success",
+                        length: data?.length,
+                        data: data,
+                    });
+                }
+            );
+        });
+    } catch (error) {
+        return next(new AppError(error.message, 500));
+    }
 }
 exports.getCourseSpecialtiesByCourseID = (req, res, next) => { //current course specialty by course ID
-    if (!req.params.courseID)
-        return next(new AppError("Course ID specified", 404));
+    try {
+        verifyToken(req, res, () => {
+            if (!req.params.courseID)
+                return next(new AppError("Course ID specified", 404));
 
-    const courseID = req.params.courseID;
+            const courseID = req.params.courseID;
 
-    const queryString = `
-    select sp.ID, sp.description
-    from specialties sp
-    where sp.course_ID = ?;
-    `;
+            const queryString = `
+            select sp.ID, sp.description
+            from specialties sp
+            where sp.course_ID = ?;
+            `;
 
-    conn.query(
-        queryString,
-        [courseID],
-        function (err, data, fields) {
-            if (err) return next(new AppError(err, 500));
-            res.status(200).json({
-                status: "success",
-                length: data?.length,
-                data: data,
-            });
-        }
-    );
+            conn.query(
+                queryString,
+                [courseID],
+                function (err, data, fields) {
+                    if (err) return next(new AppError(err, 500));
+                    res.status(200).json({
+                        status: "success",
+                        length: data?.length,
+                        data: data,
+                    });
+                }
+            );
+        });
+    } catch (error) {
+        return next(new AppError(error.message, 500));
+    }
 }
 
 exports.getSpecialtyName = (req, res, next) => { //specialty description according to its ID
+    try {
+        verifyToken(req, res, () => {
+            conn.query("SELECT * from specialties where ID = ?;",
+                [req.params.specialtyNo],
+                function (err, data, fields) {
+                    if (err) return next(new AppError(err, 500));
+                    res.status(200).json({
+                        status: "success",
+                        length: data?.length,
+                        data: data,
+                    });
+                }
+            );
+        });
+    } catch (error) {
+        return next(new AppError(error.message, 500));
+    }
 
-    conn.query("SELECT * from specialties where ID = ?;",
-        [req.params.specialtyNo],
-        function (err, data, fields) {
-            if (err) return next(new AppError(err, 500));
-            res.status(200).json({
-                status: "success",
-                length: data?.length,
-                data: data,
-            });
-        }
-    );
 }
