b8453cc9566b2102644dd498d37d941486701398
Ece Havanci <ecehavanci@gmail.com>
2024-02-15 10:53:55 +0300

    empty token redirect added to verifyToken

--- utils/verifyToken.js
+++ utils/verifyToken.js
@@ -7,6 +7,7 @@ const verifyToken = (req, res, next) => {
     const token = authHeader && authHeader.split(' ')[1];
 
     if (!token) {
+        res.redirect('https://www.ieu.edu.tr/tr');
         return res.status(401).json({ message: 'No token provided' });
     }
     try {
